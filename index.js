f9744993c693b82826b29081c2889ea920c382e5
esther <esther.githinji@student.moringaschool.com>
2022-12-16 13:39:05 +0300

    comleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+const { Body } = require("node-fetch");
+
+function submitData (){
+    fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type":"application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            name:"Steve",
+            email:"steve@steve.com"
+        })
+    })
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(object){
+        document.body.innerHTML = object["id"]
+    })
+    .catch(function(error){
+         document.body.innerHTML = error.message
+    })
+}
+ 
+ submitData()
